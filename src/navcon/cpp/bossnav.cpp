20e4fb8e333cc3c15a30c16aef1692ca02d68d5b
kartikay <kartikaybagla@gmail.com>
2018-10-15 23:04:48 +0530

    made bossnav and bosssoft live happy together

--- src/navcon/cpp/bossnav.cpp
+++ src/navcon/cpp/bossnav.cpp
@@ -1,4 +1,3 @@
-
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Int16.h"
@@ -7,132 +6,140 @@
 #include <iostream>
 #include <navcon/tval.h>
 #include <softcon/oil.h>
+#include <softcon/tasksTP.h>
+#include <softcon/tasksVD.h>
 
 using namespace std;
 using namespace ros;
 using namespace std_msgs;
 
-#define offset_end 2        			//to be changed
-float initial_ldistance =float(8) ;		//to be changed
-int pointer=1,ppointer=0;
-int point,prpoint,ourownvalue=5;
+#define offset_end 2                //to be changed
+float initial_ldistance = float(8); //to be changed
+int pointer = 1, ppointer = 0;
+int point, prpoint, ourownvalue = 5;
 float ldistance = initial_ldistance;
 Float64 rangle;
 int typecast;
-softcon::oil receivel;
+softcon::tasksTP receivel;
 std_msgs::Float64 test;
 //int offset = 0; // change this when using offset
 //Int16 kite=1;
 
-navcon :: tval sendl;
-float offset(int &pointer, int &ppointer,float &ldistance)// ldistance can be used to regulate thruster values as it is decreasing over time, and goes negative once AUV goes to opposite side of desired center
+navcon::tval sendl;
+
+float offset(int &pointer, int &ppointer, float &ldistance)
 {
-    cout<<"LDist : "<<ldistance<<endl;
-    if(ldistance < offset_end)
+    // ldistance can be used to regulate thruster values as it is decreasing over time,
+    // and goes negative once AUV goes to opposite side of desired center
+    cout << "LDist : " << ldistance << endl;
+
+    if (ldistance < offset_end)
     {
-        ldistance=initial_ldistance;
-        pointer=1;
-        ppointer=0;
-        return 0;
-    }
-    if(pointer==0)
-    {
-        ldistance=initial_ldistance;
-        pointer=1;
-        ppointer=0;
+        ldistance = initial_ldistance;
+        pointer = 1;
+        ppointer = 0;
         return 0;
     }
 
-    if(ppointer==pointer)
-        {
-            return pointer*ldistance;
-        }
+    if (pointer == 0)
+    {
+        ldistance = initial_ldistance;
+        pointer = 1;
+        ppointer = 0;
+        return 0;
+    }
+
+    if (ppointer == pointer)
+    {
+        return pointer * ldistance;
+    }
+
     else
-        {
-            ppointer=pointer;
-            ldistance=ldistance/2;  //to be changed
-            return pointer*ldistance;
-        }
+    {
+        ppointer = pointer;
+        ldistance = ldistance / 2; //to be changed
+        return pointer * ldistance;
+    }
 }
 
-void callback(const softcon::oil& msg)
-	{
-  double kite = msg.depth;
-	receivel.depth=kite;
-  test.data=kite;
-  //typecast=receivel.depth;
-	receivel.xcor=msg.xcor;
-  //Float64 typecastxcor=receivxcor;
-	receivel.speed=msg.speed;
-  //Float64 typecastspeed=receivel.speed;
-	}
+void callback(const softcon::tasksTP &msg)
+{
+    double kite = msg.depth_change;
+    receivel.depth_change = kite;
+    test.data = kite;
+    //typecast=receivel.depth;
+    receivel.offset = msg.offset;
+    //Float64 typecastxcor=receivxcor;
+    receivel.speed = msg.speed;
+    //Float64 typecastspeed=receivel.speed;
+}
 
+void callyaw(Float64 x)
+{
+    rangle = x;
+}
 
-  void callyaw(Float64 x)
-  	{rangle=x;
-
-  	}
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "bossnav");
-	ros::NodeHandle n;
+    ros::init(argc, argv, "bossnav");
+    ros::NodeHandle n;
 
+    Publisher depth = n.advertise<Float64>("concon/setspecs/depth", 1000);
+    Publisher yaw = n.advertise<Float64>("concon/setspecs/yaw", 1000);
+    Publisher pub = n.advertise<navcon::tval>("navcon/guide", 1000);
 
-Publisher depth = n.advertise<Float64>("concon/setspecs/depth", 1000);
-Publisher yaw = n.advertise<Float64>("concon/setspecs/yaw", 1000);
-Publisher pub = n.advertise<navcon::tval>("navcon/guide", 1000);
-
-ROS_INFO_STREAM("Navigation boss is running");
-Subscriber subyaw = n.subscribe("softcon/setyaw",1000,&callyaw);
-Subscriber sub = n.subscribe("softcon/lantern", 1000, &callback);
-Rate rate(10);
-while (ok())
-	{
-
-	//<------------------Finding Thruster values for depth----------------------->
-			sendl.depthright=1500+200*receivel.depth;  //review NEEDED
-			sendl.depthleft=1500+200*receivel.depth;   //review -- set value for ourownvalue depending on testing
-
-
-// <------------------- Finding Thruster values for offyaw------------------->
-if (offset)
+    ROS_INFO_STREAM("Navigation boss is running");
+    Subscriber subyaw = n.subscribe("softcon/setyaw", 1000, &callyaw);
+    Subscriber sub = n.subscribe("softcon/lantern", 1000, &callback);
+    Rate rate(10);
+    while (ok())
     {
-	 	  sendl.offyawfront=1500+100/ldistance; //review
-			sendl.offyawback=1500-100/ldistance; //review
+        //<------------------Finding Thruster values for depth----------------------->
+        sendl.depthright = 1500 + 200 * receivel.depth_change; //review NEEDED
+        sendl.depthleft = 1500 + 200 * receivel.depth_change;  //review -- set value for ourownvalue depending on testing
+
+        // <------------------- Finding Thruster values for offyaw------------------->
+        if (offset)
+        {
+            sendl.offyawfront = 1500 + 100 / ldistance; //review
+            sendl.offyawback = 1500 - 100 / ldistance;  //review
+        }
+
+        else
+        {
+            sendl.offyawfront = 1500;
+            sendl.offyawback = 1500;
+        }
+
+        // <------------------- Finding Thruster values for speed------------------->
+        sendl.speedleft = 1500 + 100 * receivel.speed;
+        sendl.speedright = 1500 + 100 * receivel.speed;
+
+        //ldistance=offset(point,prpoint,initial_ldistance);
+        int shift_x = 300 - receivel.offset;
+        if (shift_x < 0)
+        {
+            shift_x = -1;
+        }
+        else if (shift_x == 0)
+        {
+            shift_x = 0;
+        }
+        else
+        {
+            shift_x = 1;
+        }
+        point = shift_x;
+        pub.publish(sendl);
+        depth.publish(test);
+        yaw.publish(rangle);
+
+        //For debugging purposes
+        ROS_INFO_STREAM(sendl);
+        ROS_INFO_STREAM(rangle);
+        ROS_INFO_STREAM(test);
+        spinOnce();
+        rate.sleep();
     }
-  else
-      sendl.offyawfront=1500;
-      sendl.offyawback=1500;
-// <------------------- Finding Thruster values for speed------------------->
-      sendl.speedleft=1500 + 100*receivel.speed;
-      sendl.speedright=1500 + 100*receivel.speed;
-
-      //ldistance=offset(point,prpoint,initial_ldistance);
-			int shift_x = 300-receivel.xcor;
-			if (shift_x < 0)
-				{
-				shift_x = -1;
-				}
-			else if (shift_x == 0)
-			{
-				shift_x=0;
-			}
-			else
-			{
-					shift_x = 1;
-			}
-			point=shift_x;
-			pub.publish(sendl);
-      depth.publish(test);
-      yaw.publish(rangle);
-
-      //For debugging purposes
-      ROS_INFO_STREAM(sendl);
-      ROS_INFO_STREAM(rangle);
-      ROS_INFO_STREAM(test);
-	spinOnce();
-	rate.sleep();
-}
-	return 0;
-
+    return 0;
 }
--- src/softcon/cpp/Boss_Soft.cpp
+++ src/softcon/cpp/Boss_Soft.cpp
@@ -9,10 +9,9 @@ using namespace std;
 using namespace ros;
 using namespace std_msgs;
 float currentdepth, refdepth, relative_depth;
-int flag=0;
+int flag = 0;
 softcon::tasksVD receivel;
 softcon::tasksTP sendl;
-
 
 void callcor(softcon::tasksVD wtf)
 {
@@ -25,59 +24,55 @@ void callcor(softcon::tasksVD wtf)
 
 void calldepth(const Float64 x)
 {
-  cout<<"current depth is  "<<x<<"\n";
-  currentdepth=x.data;
+  cout << "current depth is  " << x << "\n";
+  currentdepth = x.data;
 }
 
 int main(int argc, char **argv)
 {
-  init(argc, argv, "Software Boss");
-  cout<<"Software Boss is Running\n";
+  init(argc, argv, "Software_Boss");
+  cout << "Software Boss is Running\n";
   NodeHandle n;
   Publisher pub = n.advertise<softcon::tasksTP>("softcon/lantern", 1000);
-  Subscriber subdepth = n.subscribe("concon/statespecs/depth", 1000, &calldepth) ;
-  Subscriber subtask = n.subscribe("softcon/task", 1000, &callcor) ;
+  Subscriber subdepth = n.subscribe("concon/statespecs/depth", 1000, &calldepth);
+  Subscriber subtask = n.subscribe("softcon/task", 1000, &callcor);
   Rate rate(10);
 
   int count = 0;
   while (ok())
   {
-    if (count==5)
+    if (count == 5)
     {
-      refdepth=currentdepth;                          // setting current value from Depth sensor at the surface of the pool as the reference depth
-    }
-    
-    relative_depth=currentdepth+refdepth;
-
-
-    if(relative_depth<3)                  //change in depth between pool surface and water surface,set threshold to turn on thrusters
-    {
-      sendl.depth_change=0;
-    }
-    else if (relative_depth==3)               //to maintain a particular depth,thrusters running
-    {
-      sendl.depth_change=0;
-    }
-    else if(relative_depth<3 && flag==0)      //to attain a particular Depth,thrusters running
-    {
-      sendl.depth_change=1;
+      refdepth = currentdepth; // setting current value from Depth sensor at the surface of the pool as the reference depth
     }
 
+    relative_depth = currentdepth + refdepth;
 
+    if (relative_depth < 3) //change in depth between pool surface and water surface,set threshold to turn on thrusters
+    {
+      sendl.depth_change = 0;
+    }
+    else if (relative_depth == 3) //to maintain a particular depth,thrusters running
+    {
+      sendl.depth_change = 0;
+    }
+    else if (relative_depth < 3 && flag == 0) //to attain a particular Depth,thrusters running
+    {
+      sendl.depth_change = 1;
+    }
 
     if (flag) // sendl to navcon, if values are receiveld from a task node
-    { 
-      cout<<"workng";
-      sendl.depth_change=receivel.depth_setpoint;
-      sendl.yaw_change=receivel.yaw_setpoint;
-      sendl.speed=receivel.speed_change;
+    {
+      cout << "workng";
+      sendl.depth_change = receivel.depth_setpoint;
+      sendl.yaw_change = receivel.yaw_setpoint;
+      sendl.speed = receivel.speed_change;
 
       sendl.offset = 100 * receivel.thrust_factor;
-      flag=0;
-
+      flag = 0;
     }
 
-    ROS_INFO_STREAM( sendl);
+    ROS_INFO_STREAM(sendl);
     pub.publish(sendl);
     spinOnce();
     rate.sleep();
